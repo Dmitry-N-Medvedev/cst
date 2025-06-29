67b9981d086817914d028bdecad4fdb29dfaab52
Dmitry Medvedev <dmitry.medvedev@gmail.coom>
2025-06-29 12:17:45 +0200

    added build and test from the root

--- build.zig
+++ build.zig
@@ -2,12 +2,13 @@ const std = @import("std");
 // const addTests = @import("utils").addTests;
 
 pub fn build(b: *std.Build) void {
-    // const target = b.standardTargetOptions(.{});
-    // const optimize = b.standardOptimizeOption(.{});
-    // const test_step = b.step("test", "Run all tests");
+    // specs
+    const hdr_parser_specs = b.addSystemCommand(&.{ "zig", "build", "test" });
+    hdr_parser_specs.cwd = b.path("src/hdr/");
 
-    // addTests(b, target, optimize, test_step, "src") catch |err| {
-    //     std.log.err("failed to add tests for hdr: {}", .{err});
-    //     std.process.exit(1);
-    // };
+    // bin
+    const hdr_parser_bin = b.addSystemCommand(&.{ "zig", "build" });
+    hdr_parser_bin.cwd = b.path("src/hdr/");
+    b.getInstallStep().dependOn(&hdr_parser_specs.step);
+    b.getInstallStep().dependOn(&hdr_parser_bin.step);
 }
