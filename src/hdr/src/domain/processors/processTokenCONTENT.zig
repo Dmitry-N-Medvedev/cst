62f1acfe943f9f6132d3f67a560cec9827dfeecf
Dmitry Medvedev <dmitry.medvedev@gmail.coom>
2025-06-30 11:36:47 +0200

    processTokenContent

---
+++ src/hdr/src/domain/processors/processTokenCONTENT.zig
@@ -0,0 +1,35 @@
+const std = @import("std");
+const parseSingleLineSingleStringValue = @import("../parsers/parseSingleLineSingleStringValue.zig").parseSingleLineSingleStringValue;
+const Token = @import("../Token.zig").Token;
+const TokenError = @import("../Token.zig").TokenError;
+const token = @tagName(Token.CONTENT);
+
+pub fn processTokenCONTENT(line: []const u8) ![]const u8 {
+    const result = try parseSingleLineSingleStringValue(token, line);
+
+    if (result.len == 0) {
+        return TokenError.EmptyValue;
+    }
+
+    return result;
+}
+
+test "OK" {
+    const expected_value = "START";
+    const line = @tagName(Token.CONTENT) ++ " \t  '" ++ expected_value ++ "'";
+    const actualTokenContentValue = try processTokenCONTENT(line);
+
+    try std.testing.expectEqualSlices(u8, expected_value, actualTokenContentValue);
+}
+
+test "fail on empty value" {
+    const line = @tagName(Token.CONTENT);
+
+    try std.testing.expectError(TokenError.EmptyValue, processTokenCONTENT(line));
+}
+
+test "fail on KeyNotFound" {
+    const line = "RANDOM_KEY \t START";
+
+    try std.testing.expectError(TokenError.KeyNotFound, processTokenCONTENT(line));
+}
