8068fd1c3b2fd953d5fecaaf443c831794cfe882
Dmitry Medvedev <dmitry.medvedev@gmail.coom>
2025-06-30 14:55:43 +0200

    processTokenGenlab

---
+++ src/hdr/src/domain/processors/processTokenGENLAB.zig
@@ -0,0 +1,35 @@
+const std = @import("std");
+const parseSingleLineSingleStringValue = @import("../parsers/parseSingleLineSingleStringValue.zig").parseSingleLineSingleStringValue;
+const Token = @import("../Token.zig").Token;
+const TokenError = @import("../Token.zig").TokenError;
+const token = @tagName(Token.GENLAB);
+
+pub fn processTokenGENLAB(line: []const u8) ![]const u8 {
+    const result = try parseSingleLineSingleStringValue(token, line);
+
+    if (result.len == 0) {
+        return TokenError.EmptyValue;
+    }
+
+    return result;
+}
+
+test "OK" {
+    const expected_value = "Blade 1 Loads: Root axes";
+    const line = @tagName(Token.GENLAB) ++ " \t  '" ++ expected_value ++ "'";
+    const actualTokenConfigValue = try processTokenGENLAB(line);
+
+    try std.testing.expectEqualSlices(u8, expected_value, actualTokenConfigValue);
+}
+
+test "fail on empty value" {
+    const line = @tagName(Token.GENLAB);
+
+    try std.testing.expectError(TokenError.EmptyValue, processTokenGENLAB(line));
+}
+
+test "fail on KeyNotFound" {
+    const line = "RANDOM_KEY \t 'something irrelevant'";
+
+    try std.testing.expectError(TokenError.KeyNotFound, processTokenGENLAB(line));
+}
