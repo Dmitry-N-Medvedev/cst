a7abbbbaf55a88a21d323af98ae3736a23320509
Dmitry Medvedev <dmitry.medvedev@gmail.coom>
2025-06-30 11:40:36 +0200

    processTokenConfig

---
+++ src/hdr/src/domain/processors/processTokenCONFIG.zig
@@ -0,0 +1,35 @@
+const std = @import("std");
+const parseSingleLineSingleStringValue = @import("../parsers/parseSingleLineSingleStringValue.zig").parseSingleLineSingleStringValue;
+const Token = @import("../Token.zig").Token;
+const TokenError = @import("../Token.zig").TokenError;
+const token = @tagName(Token.CONFIG);
+
+pub fn processTokenCONFIG(line: []const u8) ![]const u8 {
+    const result = try parseSingleLineSingleStringValue(token, line);
+
+    if (result.len == 0) {
+        return TokenError.EmptyValue;
+    }
+
+    return result;
+}
+
+test "OK" {
+    const expected_value = "TRANSIENT";
+    const line = @tagName(Token.CONFIG) ++ " \t  '" ++ expected_value ++ "'";
+    const actualTokenConfigValue = try processTokenCONFIG(line);
+
+    try std.testing.expectEqualSlices(u8, expected_value, actualTokenConfigValue);
+}
+
+test "fail on empty value" {
+    const line = @tagName(Token.CONFIG);
+
+    try std.testing.expectError(TokenError.EmptyValue, processTokenCONFIG(line));
+}
+
+test "fail on KeyNotFound" {
+    const line = "RANDOM_KEY \t 'TRANSIENT'";
+
+    try std.testing.expectError(TokenError.KeyNotFound, processTokenCONFIG(line));
+}
